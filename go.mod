1653948ffb535510989862d296f85f3c08e8aa53
STaninnat <taninnat.s@gmailcom>
2024-09-27 22:00:22 +0700

    create config file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/STaninnat/rss_gator
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/STaninnat/rss_gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("field")
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
