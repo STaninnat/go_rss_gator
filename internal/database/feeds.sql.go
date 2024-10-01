10898fa9b4f04e6a3a2f846031961858ef1684f5
STaninnat <taninnat.s@gmailcom>
2024-10-01 19:29:51 +0700

    add follow and following commands to create a feed follow record for current user and print all the names of the feeds the current user is following respectively

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -48,8 +48,27 @@ func (q *Queries) CreatFeed(ctx context.Context, arg CreatFeedParams) (Feed, err
 	return i, err
 }
 
+const getFeedByURL = `-- name: GetFeedByURL :one
+SELECT id, created_at, updated_at, name, url, user_id FROM feeds
+WHERE url = $1
+`
+
+func (q *Queries) GetFeedByURL(ctx context.Context, url string) (Feed, error) {
+	row := q.db.QueryRowContext(ctx, getFeedByURL, url)
+	var i Feed
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+		&i.Url,
+		&i.UserID,
+	)
+	return i, err
+}
+
 const getFeeds = `-- name: GetFeeds :many
-SELECT id, created_at, updated_at, name, url, user_id FROM  feeds
+SELECT id, created_at, updated_at, name, url, user_id FROM feeds
 `
 
 func (q *Queries) GetFeeds(ctx context.Context) ([]Feed, error) {
